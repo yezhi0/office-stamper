61c10190eac450a54d457924403085c2172b9c9f
leaves <13081541293@163.com>
2026-04-27 21:32:32 +0800

    Create EMFMatada.java

---
+++ imageio-emf/src/main/java/pro/verron/officestamper/imageio/emf/EMFMatada.java
@@ -0,0 +1,67 @@
+package pro.verron.officestamper.imageio.emf;
+
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.metadata.IIOMetadataNode;
+import javax.imageio.metadata.IIOMetadataFormat;
+import org.w3c.dom.Node;
+
+class EMFMetadata extends IIOMetadata {
+    private final int width;
+    private final int height;
+
+    EMFMetadata(int width, int height) {
+        super(false,
+              "javax_imageio_1.0",
+              null,
+              null, null);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public Node getAsTree(String formatName) {
+        if (formatName.equals(nativeMetadataFormatName)) {
+            IIOMetadataNode root = new IIOMetadataNode(nativeMetadataFormatName);
+            IIOMetadataNode dimension = new IIOMetadataNode("Dimension");
+
+            IIOMetadataNode widthNode = new IIOMetadataNode("PixelWidth");
+            widthNode.setAttribute("value", Integer.toString(width));
+
+            IIOMetadataNode heightNode = new IIOMetadataNode("PixelHeight");
+            heightNode.setAttribute("value", Integer.toString(height));
+
+            dimension.appendChild(widthNode);
+            dimension.appendChild(heightNode);
+            root.appendChild(dimension);
+            return root;
+        }
+        throw new IllegalArgumentException("Unsupported format: " + formatName);
+    }
+
+    @Override
+    public boolean isReadOnly() { return true; }
+
+    @Override
+    public IIOMetadataFormat getMetadataFormat() { return null; }
+
+    @Override
+    public void reset() { }
+
+    @Override
+    public IIOMetadataNode getAsTree() {
+        return (IIOMetadataNode) getAsTree(nativeMetadataFormatName);
+    }
+
+    @Override
+    public boolean isStandardMetadataFormatSupported() { return true; }
+
+    public String getNativeMetadataFormatName() { return nativeMetadataFormatName; }
+
+    public void setFromTree(String formatName, Node root) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void mergeTree(String formatName, Node root) {
+        throw new UnsupportedOperationException();
+    }
+}
